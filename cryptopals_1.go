b8b5a7a9c1c4ab2558f41b196a8268e8887b494b
Ellen <eyusti@gmail.com>
2020-04-01 16:08:52 -0400

    Cryptopals problem 4

--- cryptopals_1.go
+++ cryptopals_1.go
@@ -4,6 +4,8 @@ import "fmt"
 import "encoding/hex"
 import "encoding/base64"
 import "strings"
+import "os"
+import "bufio"
 
 
 func main(){
@@ -17,18 +19,24 @@ func main(){
 	//solution_2 := problem_2(one, two)
 	//fmt.Println(solution_2)
 	// Problem 3
-	const secret = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-	solution_3:= problem_3(secret)
-	fmt.Println(solution_3)
-	//solution_3 := problem_3(secret)
+	//const secret = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
+	//solution_3, _:= problem_3(secret)
 	//fmt.Println(solution_3)
+	//Problem 4
+	const file_name = "set_1_problem_4.txt"
+	solution_4:= problem_4(file_name)
+	fmt.Println(solution_4)
+}
+
+func error_checker(e error){
+	if e != nil {
+		fmt.Println("** WAS ERROR **")
+	}
 }
 
 func problem_1(input string)string{
 	bytes, err := hex.DecodeString(input)
-	if err != nil {
-		fmt.Println("** WAS ERROR **")
-	}
+	error_checker(err)
 	solution := base64.StdEncoding.EncodeToString(bytes)
 	return solution
 }
@@ -37,9 +45,9 @@ func problem_2(input_1, input_2 string)string{
 	bytes_1, err_1 := hex.DecodeString(input_1)
 	bytes_2, err_2 := hex.DecodeString(input_2)
 
-	if err_1 != nil|| err_2 != nil {
-		fmt.Println("** WAS ERROR **")
-	}
+	error_checker(err_1)
+	error_checker(err_2)
+
 	length := len(bytes_1)
 	var result []byte
 	for b:= 0; b < length; b++ {
@@ -50,11 +58,9 @@ func problem_2(input_1, input_2 string)string{
 	return result_encoded
 }
 
-func problem_3(input string)string{
+func problem_3(input string)(string, int){
 	working_bytes, err := hex.DecodeString(input)
-	if err != nil {
-		fmt.Println("** WAS ERROR **")
-	}
+	error_checker(err)
 
 	var decoded []byte
 	c_score := 0
@@ -69,6 +75,8 @@ func problem_3(input string)string{
 		c_score += strings.Count(string(decoded),"i")
 		c_score += strings.Count(string(decoded),"o")
 		c_score += strings.Count(string(decoded),"u")
+		c_score += strings.Count(string(decoded)," ")
+		c_score -= strings.Count(string(decoded),"\n")
 		if c_score > h_score{
 			solution = string(decoded)
 			h_score = c_score
@@ -79,5 +87,30 @@ func problem_3(input string)string{
 		decoded = nil
 		c_score = 0
 	}
-	return solution
+	return solution, h_score
+}
+
+func problem_4(input string)string{
+	file, err := os.Open(input)
+	error_checker(err)
+	defer file.Close()
+	
+	var hex_strings []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan(){
+		hex_strings = append(hex_strings, scanner.Text())
+	}
+
+	h_score := 0
+	var best_suggestion string
+	for i:= 0; i<len(hex_strings); i++ {
+		suggestion, score := problem_3(hex_strings[i])
+		//fmt.Println(suggestion)
+		//fmt.Println(score)
+		if score > h_score {
+			h_score = score
+			best_suggestion = suggestion
+		}
+	}
+	return best_suggestion
 }
